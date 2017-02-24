0bfd387f4e48206c9442ef583f95dcf81e7e512d
Rodolphe Redoute <rodolphe.redoute@devinci.fr>
2017-02-24 15:35:05 +0100

    update test
    dupdating the tests

--- weather/tests/tests.js
+++ weather/tests/tests.js
@@ -3,9 +3,22 @@
 const chai = require('chai');
 chai.should();
 
+var assert = require('assert');
+const meow = require('meow');
+const chalk = require('chalk');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
 const weather = require('../index.js');
 
 describe('Weather', () => {
-	it('should get the weather', (done) => {
+	it('should get the weather', function() {
+		assert.notEqual(undefined, meow);
+    	assert.notEqual(undefined, chalk);
+    	assert.notEqual(undefined, updateNotifier);
 	});
+
+	describe("linkTo", function(){
+		expect(pkg.linkTo).to.exist;
+	})
 });
